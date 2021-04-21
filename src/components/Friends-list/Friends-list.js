758fda7c1123de894211441a472e9fb06e7045b8
OleksandrBalashov <aleksandr.balashov.sax@gmal.com>
2021-04-21 23:21:43 +0300

    last

--- src/components/Friends-list/Friends-list.js
+++ src/components/Friends-list/Friends-list.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Friends-list.module.css';
 
+
 const FriendsList = ({ friends }) => {
     return (
         <ul className={styles.list}>
